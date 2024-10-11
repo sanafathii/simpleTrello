2a06a84c8c0411eb9d110ae936005b49841501dd
sana <sana.fathi30@gmail.com>
2024-10-11 15:11:24 +0330

    fix bug

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,27 +5,28 @@ import { verifyPassword } from "../../../utils/auth";
 import connectDB from "../../../utils/connectDB";
 
 const authOptions = {
-    session: { strategy: "jwt" },
-    providers: [
-        CredentialsProvider({
-            async authorize(credentials, req) {
-                const { email, password } = credentials;
-                try {
-                    await connectDB();
-                } catch (error) {
-                    throw new Error("Error in connecting to DB!");
-                }
-                if (!email || !password) {
-                    throw new Error("Invalid Data!");
-                }
-                const user = await User.findOne({ email: email });
-                if (!user) throw new Error("User doesn't exist!");
-                const isValid = await verifyPassword(password, user.password);
-                if (!isValid) throw new Error("Username or password is incorrect!");
-                return { email };
-            },
-        }),
-    ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: { strategy: "jwt" },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials, req) {
+        const { email, password } = credentials;
+        try {
+          await connectDB();
+        } catch (error) {
+          throw new Error("Error in connecting to DB!");
+        }
+        if (!email || !password) {
+          throw new Error("Invalid Data!");
+        }
+        const user = await User.findOne({ email: email });
+        if (!user) throw new Error("User doesn't exist!");
+        const isValid = await verifyPassword(password, user.password);
+        if (!isValid) throw new Error("Username or password is incorrect!");
+        return { email };
+      },
+    }),
+  ],
 };
 
 export default NextAuth(authOptions);
