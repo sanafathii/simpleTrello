5765d5a4f893db9965b0a81015e11ac17f5ea38c
sana <sana.fathi30@gmail.com>
2024-10-11 14:57:01 +0330

    added logout

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,14 +1,27 @@
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 import { BiMessageSquareAdd } from "react-icons/bi";
 import { RxDashboard } from "react-icons/rx";
 import { VscListSelection } from "react-icons/vsc";
+import { FiLogOut } from "react-icons/fi";
 
 function Layout({ children }) {
+  const { status } = useSession();
+
+  const logoutHandler = async () => {
+    signOut();
+  };
   return (
     <div className="container">
       <header>
         <p>Fullstack Todo App</p>
+        {status === "authenticated" ? (
+          <button onClick={logoutHandler}>
+            Logout
+            <FiLogOut />
+          </button>
+        ) : null}
       </header>
       <div className="container--main">
         <aside>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,22 @@
+import { getSession } from "next-auth/react";
 import HomePage from "../components/template/HomePage";
 
 export default function Home() {
   return (
     <div>
-      <h1>todo App</h1>
       <HomePage />
     </div>
   );
+}
+
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
+  if (!session) {
+    return {
+      redirect: { destination: "/signin", permanent: false },
+    };
+  }
+  return {
+    props: {},
+  };
 }
